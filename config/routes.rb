eba15c2579b86a3465623a2be7b81f1b2e14c5e9
UwUbulachka <meuw25@gmail.com>
2021-11-24 15:29:48 +0500

    Adding routs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root              'pages#home'
   get 'about'   => 'pages#about'
   get 'contact' => 'pages#contact'
+  get 'signup'  => 'users#new'
+  resources :users
 end
