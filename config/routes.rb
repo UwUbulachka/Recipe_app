f243a2ca639f9b9b4b2ac8cd1fab887639176e17
UwUbulachka <meuw25@gmail.com>
2021-11-25 16:29:27 +0500

    Creating controller Sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  root              'pages#home'
+  root             'pages#home'
   get 'about'   => 'pages#about'
   get 'contact' => 'pages#contact'
   get 'signup'  => 'users#new'
+  get 'login'   => 'sessions#new'
+  get 'login'   => 'sessions#create'
+  get 'logout'  => 'sessions#destroy'
+  
   resources :users
 end
