08c376cea61293a914587edb94a899308bff9025
UwUbulachka <meuw25@gmail.com>
2021-11-25 14:20:48 +0500

    Testing successful registration

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -7,5 +7,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       post users_path, params: {user: {name: "", email: "user@invalid", password: "foo", password_confirmation: "bar"} }
     end  
     assert_template 'users/new'
+    assert_select 'div#error_explanation' 
+    assert_select 'div.field_with_errors'
   end  
+
+  test "Successful registration" do 
+    get signup_path
+    assert_difference 'User.count', 1 do 
+      post users_path, params: {user: {name: "Sasha", email: "sasha@exemple.com", password: "foobar", password_confirmation: "foobar"} }
+    end  
+    follow_redirect! 
+    assert_template 'users/show'
+    assert_not flash.nil?
+  end
 end
