5c92c3fd11beac82ac7968f0735b3cf54ee1672d
UwUbulachka <meuw25@gmail.com>
2021-11-26 23:15:17 +0500

    Adding column remember_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_24_110016) do
+ActiveRecord::Schema.define(version: 2021_11_26_120808) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_11_24_110016) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "avatar"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
