82c5ebec65f0c8ecec4b4f90cb327e077b197575
UwUbulachka <meuw25@gmail.com>
2021-11-28 22:22:33 +0500

    Requiring user rights

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def show 
     @user = User.find(params[:id])
@@ -40,10 +41,18 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :avatar)
   end
 
+  # Подтверждает вход пользователя.
   def logged_in_user 
     unless logged_in?
       redirect_to login_url
       flash[:danger] = "Сначала осуществите вход" 
     end   
   end
+
+  # Подтверждает права пользователя.
+  def correct_user
+    @user = User.find(params[:id]) 
+    redirect_to root_url unless current_user?(@user)
+  end
+
 end
