67128c6f0198e4337095e350434e646c8a7d3c0b
UwUbulachka <meuw25@gmail.com>
2021-11-28 21:04:12 +0500

    Login testing

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -7,6 +7,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end  
   
   test "unsuccessful edit" do 
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     assert_no_difference 'User.count' do 
@@ -18,12 +19,13 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end 
 
     test "successful edit" do 
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     name = "Dasha"
     email = "foo@bar.com"  
     patch user_path(@user), params: {user: {name: name, email: email, password: "", password_confirmation: "", avatar: 'kot.png'} } 
-    assert_not flash.nil?
+    assert_not flash.empty?
     assert_redirected_to @user 
     @user.reload
     assert_equal @user.name, name
