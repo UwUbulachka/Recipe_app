c832dde4b667f41baa7de4a2c9cc64e98fc6c9e8
UwUbulachka <meuw25@gmail.com>
2021-11-28 22:23:05 +0500

    Testing user rights

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,6 +4,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   
   def setup
   	@user = users(:sasha)
+  	@oser = users(:masha)
   end
 
   test "should get signup" do
@@ -18,9 +19,22 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end 	
 
   test "should redirect update when not logged in" do
-    patch user_path(@user), params: {id: @user}
+    patch user_path(@user), params: {id: @user, email: @user.email, avatar: @user.avatar }
     assert_not flash.empty?
     assert_redirected_to login_url
   end
 
+  test "should redirect edit when logged in as wrong user" do 
+    log_in_as(@user)
+    get edit_user_path(@oser), params: {id: @user}
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+    test "should redirect update when logged in as wrong user" do 
+    log_in_as(@oser)
+    patch user_path(@user), params: {id: @user, email: @user.email, avatar: @user.avatar}
+    assert flash.empty?
+    assert_redirected_to root_url
+  end	
 end
