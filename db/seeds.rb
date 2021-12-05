51e90c1d205aef8a8ec33bd4acab90f107d75e9c
UwUbulachka <meuw25@gmail.com>
2021-12-05 16:06:20 +0500

    User samples

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: "Sasha Meuw", 
+			 email: "sasha@example.com",
+			 password: "foobar",
+			 password_confirmation: "foobar",
+			 avatar: File.open(Rails.root.join('app', 'assets', 'images', 'kisa.jpg')))
+
+User.create!(name: "Lilya Mur", 
+			 email: "lilya@example.com",
+			 password: "123123",
+			 password_confirmation: "123123",
+			 avatar: File.open(Rails.root.join('app', 'assets', 'images', 'cat.jpg')))
+
+User.create!(name: "Masha UwU", 
+			 email: "masha@example.com",
+			 password: "123456",
+			 password_confirmation: "123456",
+			 avatar: File.open(Rails.root.join('app', 'assets', 'images', 'kot.jpg')))
+
+99.times do |n|
+  name = Faker::Name.name
+  email = "example-#{n+1}@exemple.org"
+  password = "password"
+  User.create!(name: name,
+			   email: email,
+			   password: password,
+			   password_confirmation: password)
+end
