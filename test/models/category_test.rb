66d8c96d18bb745105a0f0549d8d173dc3b2d266
UwUbulachka <meuw25@gmail.com>
2021-12-18 23:35:23 +0500

    Testing a category

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup 
+    @category = Category.new(name: "Мясо")
+  end 
+
+  test "category must be valid" do 
+    assert @category.valid?
+  end 
+
+  test "category should be present" do 
+    @category.name = " "
+    assert_not @category.valid?
+  end
+
+  test "category must not exceed 20 characters" do 
+    @category.name = "a" * 21
+    assert_not @category.valid?
+  end 
 end
